94a1275eb54515c4a22a1539258d53c359d3ab37
Tezla-Lee <tezla.lee@gmail.com>
2020-12-09 23:59:18 +0900

    Add solution

--- Baekjoon/src/b1753/Main.java
+++ Baekjoon/src/b1753/Main.java
@@ -3,7 +3,7 @@ package b1753;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -13,64 +13,72 @@ public class Main {
         n = Integer.parseInt(st.nextToken());
         int caseNum = Integer.parseInt(st.nextToken());
         int start = Integer.parseInt(br.readLine());
-        board = new int[n + 1][n + 1];
-        isUsed = new boolean[n + 1][n + 1];
+        board = new ArrayList[n + 1];
+        for (int i = 0; i < n + 1; i++) {
+            board[i] = new ArrayList<>();
+        }
         route = new int[n + 1];
+        Arrays.fill(route, INF);
+        check = new boolean[n + 1];
         int a, b, c;
         for (int i = 0; i < caseNum; i++) {
             st = new StringTokenizer(br.readLine());
             a = Integer.parseInt(st.nextToken());
             b = Integer.parseInt(st.nextToken());
             c = Integer.parseInt(st.nextToken());
-
-            if (board[a][b] != 0) {
-                board[a][b] = Math.min(c, board[a][b]);
-            } else {
-                board[a][b] = c;
-            }
+            board[a].add(new Node(b, c));
         }
-        dfs(start, 0);
+        bfs(start);
         for (int i = 1; i < n + 1; i++) {
-            if (i == start) {
-                sb.append(0);
+            if (route[i] == INF) {
+                sb.append("INF");
             } else {
-                if (route[i] == 0) {
-                    sb.append("INF");
-                } else {
-                    sb.append(route[i]);
-                }
+                sb.append(route[i]);
             }
             sb.append("\n");
         }
         System.out.println(sb);
     }
 
-    static int[][] board;
+
+    static final int INF = Integer.MAX_VALUE;
+    static ArrayList<Node>[] board;
     static int[] route;
     static int n;
-    static boolean[][] isUsed;
+    static boolean[] check;
 
-    static void dfs(int start, int distance) {
-        for (int i = 1; i < n + 1; i++) {
-            if (!isUsed[start][i] && board[start][i] != 0) {
-                isUsed[start][i] = true;
-//                if (route[i] == 0) {
-//                    route[i] = distance + board[start][i];
-//                } else {
-//                    route[i] = Math.min(route[i], distance + board[start][i]);
-//                }
-//                dfs(i, distance + board[start][i]);
-
-                if (route[i] == 0) {
-                    route[i] = distance + board[start][i];
-                } else {
-                    if (distance + board[start][i] < route[i]) {
-                        route[i] = distance + board[start][i];
-                        dfs(i, distance + board[start][i]);
-                    }
+    static void bfs(int start) {
+        Queue<Node> queue = new PriorityQueue<>();
+        queue.add(new Node(start, 0));
+        route[start] = 0;
+        while (!queue.isEmpty()) {
+            Node temp = queue.poll();
+            if (check[temp.index]) {
+                continue;
+            }
+            int t = temp.index;
+            check[t] = true;
+            for (Node node : board[t]) {
+                if (route[node.index] > route[t] + node.distance) {
+                    route[node.index] = route[t] + node.distance;
+                    queue.add(new Node(node.index, route[node.index]));
                 }
             }
         }
     }
 }
 
+class Node implements Comparable<Node> {
+    int index;
+    int distance;
+
+    public Node(int index, int distance) {
+        this.index = index;
+        this.distance = distance;
+    }
+
+    @Override
+    public int compareTo(Node o) {
+        return distance - o.distance;
+    }
+}
---
+++ Baekjoon/src/b2206/Main.java
@@ -0,0 +1,95 @@
+package b2206;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        map = new int[n + 1][m + 1];
+        dp = new int[n + 1][m + 1];
+        isVisited = new boolean[n + 1][m + 1][2];
+
+        for (int[] i : dp) {
+            Arrays.fill(i, Integer.MAX_VALUE);
+        }
+
+        for (int i = 1; i < n + 1; i++) {
+            String[] temp = br.readLine().split("");
+            for (int j = 1; j < m + 1; j++) {
+                map[i][j] = Integer.parseInt(temp[j - 1]);
+            }
+        }
+
+        bfs();
+    }
+
+    static int n, m;
+    static int[][] map;
+    static int[][] dp;
+    static boolean[][][] isVisited;
+    static int[] dx = {1, 0, 0, -1};
+    static int[] dy = {0, 1, -1, 0};
+    static int min = Integer.MAX_VALUE;
+
+    static void bfs() {
+        Stack<Point> queue = new Stack<>();
+        queue.add(new Point(1, 1, 0, 1));
+        isVisited[1][1][0] = true;
+        isVisited[1][1][1] = true;
+        dp[1][1] = 1;
+
+        while (!queue.isEmpty()) {
+            Point point = queue.pop();
+
+            if (point.x == n && point.y == m) {
+//                min = Math.min(min, point.count);
+                System.out.println(point.count);
+                return;
+            }
+
+            for (int i = 0; i < 4; i++) {
+                int tempX = point.x + dx[i];
+                int tempY = point.y + dy[i];
+
+                if (tempX > 0 && tempY > 0 && tempX < n + 1 && tempY < m + 1) {
+                    // 빈 공간
+                    if (map[tempX][tempY] == 0) {
+                        if (!isVisited[tempX][tempY][point.breakWall]) {
+                            isVisited[tempX][tempY][point.breakWall] = true;
+                            queue.add(new Point(tempX, tempY, point.breakWall, point.count + 1));
+//                            dp[tempX][tempY] = Math.min(dp[point.x][point.y] + 1, dp[tempX][tempY]);
+                        }
+                    } else { // 벽
+                        if (point.breakWall == 0 && !isVisited[tempX][tempY][1]) { // 벽 부신적 있는지 확인
+                            queue.add(new Point(tempX, tempY, 1, point.count + 1));
+                            isVisited[tempX][tempY][1] = true;
+//                            dp[tempX][tempY] = Math.min(dp[point.x][point.y] + 1, dp[tempX][tempY]);
+                        }
+                    }
+                }
+            }
+        }
+        System.out.println("-1");
+    }
+}
+
+class Point {
+    int x;
+    int y;
+    int breakWall;
+    int count;
+
+    public Point(int x, int y, int breakWall, int count) {
+        this.x = x;
+        this.y = y;
+        this.breakWall = breakWall;
+        this.count = count;
+    }
+}
