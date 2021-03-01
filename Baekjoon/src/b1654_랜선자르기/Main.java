0913fb67ed7bcd803b3c1b7d26a30b57cf40d1db
Tezla-Lee <tezla.lee@gmail.com>
2021-03-01 19:54:45 +0900

    resolve: 백준 1654 랜선 자르기
    이분탐색

---
+++ Baekjoon/src/b1654_랜선자르기/Main.java
@@ -0,0 +1,39 @@
+package b1654_랜선자르기;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int k = Integer.parseInt(st.nextToken());
+        int n = Integer.parseInt(st.nextToken());
+        int max = Integer.MIN_VALUE;
+        int[] lanWires = new int[k];
+        int ans = Integer.MIN_VALUE;
+        for (int i = 0; i < k; i++) {
+            lanWires[i] = Integer.parseInt(br.readLine());
+            max = Math.max(max, lanWires[i]);
+        }
+        long left = 1;
+        long right = max;
+
+        while (left <= right) {
+            long sum = 0;
+            long mid = (left + right) / 2;
+            for (int i = 0; i < k; i++) {
+                sum += lanWires[i] / mid;
+            }
+            if (sum >= n) {
+                left = mid + 1;
+                ans = (int) Math.max(ans, mid);
+            } else {
+                right = mid - 1;
+            }
+        }
+        System.out.println(ans);
+    }
+}
--- Baekjoon/src/b1654_랜선자르기/Main2.java
+++ Baekjoon/src/b1654_랜선자르기/Main2.java
@@ -1,39 +1,52 @@
-package b1654;
+package b1654_랜선자르기;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Main {
+public class Main2 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
+
         int k = Integer.parseInt(st.nextToken());
         int n = Integer.parseInt(st.nextToken());
-        int max = Integer.MIN_VALUE;
+
         int[] lanWires = new int[k];
-        int ans = Integer.MIN_VALUE;
+        int max = Integer.MIN_VALUE;
+
         for (int i = 0; i < k; i++) {
             lanWires[i] = Integer.parseInt(br.readLine());
             max = Math.max(max, lanWires[i]);
         }
+
+        System.out.println(binarySearch(lanWires, max, n));
+    }
+
+    static long binarySearch(int[] arr, int max, int n) {
         long left = 1;
+        long mid;
         long right = max;
+        int ans = 0;
 
         while (left <= right) {
+            mid = (left + right) / 2;
+            System.out.println(left + " " + mid + " " + right);
+
             long sum = 0;
-            long mid = (left + right) / 2;
-            for (int i = 0; i < k; i++) {
-                sum += lanWires[i] / mid;
+            for (int lan : arr) {
+                sum += lan / mid;
             }
-            if (sum >= n) {
+
+            if (sum < n) {
+                right = mid - 1;
+            } else {
                 left = mid + 1;
                 ans = (int) Math.max(ans, mid);
-            } else {
-                right = mid - 1;
             }
         }
-        System.out.println(ans);
+
+        return ans;
     }
 }
