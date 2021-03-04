12b9910f567ac2b13d3d8c2fea8d03ab66dfeff4
Tezla-Lee <tezla.lee@gmail.com>
2021-03-04 11:02:27 +0900

    solve: 프로그래머스 LEVEL1
    소수찾기

---
+++ Programmers/src/Level1/_소수찾기/Solution.java
@@ -0,0 +1,31 @@
+package Level1._소수찾기;
+
+import java.util.*;
+
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        boolean[] primes = new boolean[n + 1];
+        Arrays.fill(primes, true);
+        primes[0] = false;
+        primes[1] = false;
+
+        for (int i = 2; i * i < n + 1; i++) {
+            if (!primes[i * i]) {
+                continue;
+            }
+
+            for (int j = i * i; j < n + 1; j += i) {
+                primes[j] = false;
+            }
+        }
+
+        for (boolean prime : primes) {
+            if (prime) {
+                answer++;
+            }
+        }
+
+        return answer;
+    }
+}
