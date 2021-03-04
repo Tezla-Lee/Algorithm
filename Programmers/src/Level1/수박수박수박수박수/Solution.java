fdd24fe34ba624829fb2e70cf79bd03acd8a15a0
Tezla-Lee <tezla.lee@gmail.com>
2021-03-04 11:17:57 +0900

    solve: 프로그래머스 LEVEL1
    수박수박수박수

---
+++ Programmers/src/Level1/수박수박수박수박수/Solution.java
@@ -0,0 +1,17 @@
+package Level1.수박수박수박수박수;
+
+class Solution {
+    public String solution(int n) {
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < n / 2; i++) {
+            sb.append("수박");
+        }
+
+        if (n % 2 == 1) {
+            sb.append("수");
+        }
+
+        return sb.toString();
+    }
+}
