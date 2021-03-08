47fcf148e02a0796e632cd159b996c5d39288f02
Tezla-Lee <tezla.lee@gmail.com>
2021-03-08 10:56:47 +0900

    solve: 백준 2075 N번째 큰 수
    Priority Queue

--- Baekjoon/src/b2075_N번째큰수/Main.java
+++ Baekjoon/src/b2075_N번째큰수/Main.java
@@ -3,28 +3,27 @@ package b2075_N번째큰수;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.PriorityQueue;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int n;
-    static int[] nums;
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        n = Integer.parseInt(br.readLine());
-        nums = new int[n * n];
+        int n = Integer.parseInt(br.readLine());
+        Queue<Integer> queue = new PriorityQueue<>((a, b) -> b - a);
 
-        int index = 0;
         for (int i = 0; i < n; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             for (int j = 0; j < n; j++) {
-                nums[index++] = Integer.parseInt(st.nextToken());
+                queue.offer(Integer.parseInt(st.nextToken()));
             }
         }
 
-        Arrays.sort(nums);
+        for (int i = 0; i < n - 1; i++) {
+            queue.poll();
+        }
 
-        System.out.println(nums[n * n - n]);
+        System.out.println(queue.poll());
     }
 }
