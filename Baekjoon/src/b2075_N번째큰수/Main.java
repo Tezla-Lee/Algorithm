66246fd589726b1e1b2e8559c37c467bdd7ba9da
Tezla-Lee <tezla.lee@gmail.com>
2021-03-08 10:51:32 +0900

    solve: 백준 2075 N번째 큰 수
    sort로 풀음. 가장 쉬운 방법이지만 시간 오래 걸림.

---
+++ Baekjoon/src/b2075_N번째큰수/Main.java
@@ -0,0 +1,30 @@
+package b2075_N번째큰수;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int n;
+    static int[] nums;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+        nums = new int[n * n];
+
+        int index = 0;
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < n; j++) {
+                nums[index++] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        Arrays.sort(nums);
+
+        System.out.println(nums[n * n - n]);
+    }
+}
