8d3aeda7f8ce9bca9a87283bea6c33f5b8d3d4cd
Tezla-Lee <tezla.lee@gmail.com>
2021-03-14 16:19:39 +0900

    solve: 카카오 기출
    - 리눅스 쉘 구현

--- Programmers/src/극비문제/리눅스쉘/Solution.java
+++ Programmers/src/극비문제/리눅스쉘/Solution.java
@@ -118,9 +118,23 @@ public class Solution {
     }
 
     public static void main(String[] args) {
-        System.out.println(new Solution().solution(new String[]{"/", "/hello", "/hello/tmp"}, new String[]{"mkdir /abc"}));
-        System.out.println(new Solution().solution(new String[]{"/", "/hello", "/hello/tmp", "/root", "/root/bcd", "/root/abcd", "/root/abcd/etc", "/root/abcd/hello"},
-                new String[]{"mkdir /root/tmp", "cp /hello /root/tmp", "rm /hello"}));
+//        System.out.println(new Solution().solution(new String[]{"/", "/hello", "/hello/tmp"}, new String[]{"mkdir /abc"}));
+//        System.out.println(new Solution().solution(new String[]{"/", "/hello", "/hello/tmp", "/root", "/root/bcd", "/root/abcd", "/root/abcd/etc", "/root/abcd/hello"},
+//                new String[]{"mkdir /root/tmp", "cp /hello /root/tmp", "rm /hello"}));
+        // [/, /a, /a/b, /a/b/c, /a/b/c/d, /a/b/e, /a/tt, /tmp, /tmp/a, /tmp/a/b, /tmp/a/b/c, /tmp/a/b/c/d, /tmp/a/c, /tmp2, /tmp2/b, /tmp2/b/e]
+        System.out.println(new Solution().solution(new String[]{"/", "/a", "/a/b"}, new String[]{"mkdir /a/c",
+                "mkdir /a/b/e",
+                "mkdir /tmp",
+                "mkdir /a/b/c/d",
+                "cp /a /tmp",
+                "mkdir /tmp2",
+                "cp /tmp/a/b /tmp2",
+                "mkdir /a/tt",
+//                "rm /tmp/a/b/e",
+//                "rm /tmp2/b/c",
+//                "rm /a/c"
+        }));
+
     }
 }
 
