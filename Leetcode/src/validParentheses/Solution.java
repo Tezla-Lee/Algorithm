2b9213b5c55e4a9229f706c632f2ab523c1c04fa
Tezla-Lee <tezla.lee@gmail.com>
2021-03-15 13:50:23 +0900

    solve: LeetCode
    - Valid Parentheses
      - 라이브 코딩 연습

---
+++ Leetcode/src/validParentheses/Solution.java
@@ -0,0 +1,44 @@
+package validParentheses;
+
+import java.util.Stack;
+
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for (char c : s.toCharArray()) {
+            if (!stack.isEmpty()) {
+                char peek = stack.peek();
+
+                if (c == ')') {
+                    if (peek == '(') {
+                        stack.pop();
+                    } else {
+                        return false;
+                    }
+                } else if (c == '}') {
+                    if (peek == '{') {
+                        stack.pop();
+                    } else {
+                        return false;
+                    }
+                } else if (c == ']') {
+                    if (peek == '[') {
+                        stack.pop();
+                    } else {
+                        return false;
+                    }
+                } else {
+                    stack.add(c);
+                }
+            } else {
+                if (c == ')' || c == ']' || c == '}') {
+                    return false;
+                }
+                stack.add(c);
+            }
+        }
+
+        return stack.isEmpty();
+    }
+}
