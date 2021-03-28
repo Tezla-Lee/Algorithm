08190f8f85f43082ba29376d25cc0e74dac39ea3
Tezla-Lee <tezla.lee@gmail.com>
2021-03-28 22:34:29 +0900

    unsolved: Leetcode
    - Minimum Window Substring

---
+++ Leetcode/src/minimumWindowSubstring/Solution.java
@@ -0,0 +1,72 @@
+package minimumWindowSubstring;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+class Solution {
+    public String minWindow(String s, String t) {
+        char[] sChars = s.toCharArray();
+        char[] tChars = t.toCharArray();
+
+        HashMap<Character, Integer> sMap = new HashMap<>();
+        HashMap<Character, Integer> tMap = new HashMap<>();
+        HashMap<Character, Integer> check = new HashMap<>();
+        List<Character> list = new ArrayList<>();
+
+        StringBuilder sb = new StringBuilder();
+
+        for (char c : tChars) {
+            check.put(c, check.getOrDefault(c, 0) + 1);
+            tMap.put(c, tMap.getOrDefault(c, 0) + 1);
+        }
+
+        for (int i = 0; i < sChars.length; i++) {
+            list.add(sChars[i]);
+
+            if (tMap.containsKey(sChars[i])) {
+                sMap.put(sChars[i], sMap.getOrDefault(sChars[i], 0) + 1);
+
+                if (check.containsKey(sChars[i])) {
+                    int count = check.get(sChars[i]);
+
+                    if (count == 1) {
+                        check.remove(sChars[i]);
+                    } else {
+                        check.put(sChars[i], count - 1);
+                    }
+                }
+
+                if (check.size() == 0) {
+                    while (!sMap.containsKey(list.get(0)) || sMap.get(list.get(0)) > tMap.get(list.get(0))) {
+                        if (sMap.containsKey(list.get(0))) {
+                            sMap.put(list.get(0), sMap.get(list.get(0)) - 1);
+                        }
+
+                        list.remove(0);
+                    }
+                }
+            }
+        }
+
+        if (check.size() != 0) {
+            return "";
+        }
+
+        while (!tMap.containsKey(list.get(list.size() - 1))) {
+            list.remove(list.size() - 1);
+        }
+
+        while (!list.isEmpty()) {
+            sb.append(list.remove(0));
+        }
+
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Solution().minWindow("ADOBECODEBANC", "ABC"));
+        System.out.println(new Solution().minWindow("ab", "a"));
+        System.out.println(new Solution().minWindow("cabwefgewcwaefgcf", "cae"));
+    }
+}
