7364f4f66a45accca8c634e3ed10dd3744c6c8b4
Tezla-Lee <tezla.lee@gmail.com>
2021-03-29 12:05:23 +0900

    unsolved: 백준 17140
    - 이차원 배열과 연산
    - 첫 시도

---
+++ Baekjoon/src/b17140_이차원배열과연산/Main.java
@@ -0,0 +1,149 @@
+package b17140_이차원배열과연산;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int[][] board;
+    static int maxRow;
+    static int maxCol;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int r = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        board = new int[100][100];
+
+        maxRow = 3;
+        maxCol = 3;
+        int count = 0;
+
+        for (int i = 0; i < 3; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < 3; j++) {
+                board[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        while (board[r - 1][c - 1] != k && count < 100) {
+            if (maxRow >= maxCol) {
+                calculateR();
+            } else {
+                calculateC();
+            }
+            count++;
+
+            System.out.println("count " + count);
+            printBoard();
+        }
+
+        if (count == 100 && board[r - 1][c - 1] != k) {
+            System.out.println("-1");
+        } else {
+            System.out.println(count);
+        }
+    }
+
+    static void calculateR() {
+        HashMap<Integer, Integer> map;
+        PriorityQueue<Number> pq;
+
+        for (int i = 0; i < maxRow; i++) {
+            map = new HashMap<>();
+            pq = new PriorityQueue<>();
+            for (int j = 0; j < maxCol; j++) {
+                if (board[i][j] == 0) {
+                    continue;
+                }
+                map.put(board[i][j], map.getOrDefault(board[i][j], 0) + 1);
+            }
+
+            for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+                pq.add(new Number(entry.getKey(), entry.getValue()));
+            }
+
+            int col = 0;
+
+            while (!pq.isEmpty()) {
+                Number num = pq.poll();
+
+                board[i][col++] = num.value;
+                board[i][col++] = num.count;
+            }
+
+            maxCol = Math.max(maxCol, col);
+
+            while (board[i][col] != 0) {
+                board[i][col++] = 0;
+            }
+        }
+    }
+
+    static void calculateC() {
+        HashMap<Integer, Integer> map;
+        PriorityQueue<Number> pq;
+
+        for (int i = 0; i < maxCol; i++) {
+            map = new HashMap<>();
+            pq = new PriorityQueue<>();
+            for (int j = 0; j < maxRow; j++) {
+                if (board[j][i] == 0) {
+                    continue;
+                }
+                map.put(board[j][i], map.getOrDefault(board[j][i], 0) + 1);
+            }
+
+            for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+                pq.add(new Number(entry.getKey(), entry.getValue()));
+            }
+
+            int row = 0;
+
+            while (!pq.isEmpty()) {
+                Number num = pq.poll();
+                board[row++][i] = num.value;
+                board[row++][i] = num.count;
+            }
+
+            maxRow = Math.max(maxRow, row);
+
+            while (board[row][i] != 0) {
+                board[row++][i] = 0;
+            }
+        }
+    }
+
+    static void printBoard() {
+        for (int i = 0; i < maxRow; i++) {
+            for (int j = 0; j < maxCol; j++) {
+                System.out.printf("%3d", board[i][j]);
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+}
+
+class Number implements Comparable<Number> {
+    int value;
+    int count;
+
+    public Number(int value, int count) {
+        this.value = value;
+        this.count = count;
+    }
+
+    @Override
+    public int compareTo(Number o) {
+        return count == o.count ? value - o.value : count - o.count;
+    }
+}
