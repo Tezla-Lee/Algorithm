0306e15933da51d0d62a00edefddf0d360597d91
Tezla-Lee <tezla.lee@gmail.com>
2021-04-11 22:23:59 +0900

    solve: Leetcode Container With Most Water
    - Medium
    - Two Pointer

---
+++ Leetcode/src/containerWithMostWater/Solution2.java
@@ -0,0 +1,24 @@
+package containerWithMostWater;
+
+public class Solution2 {
+    public int maxArea(int[] height) {
+        int water = 0;
+        int left = 0;
+        int right = height.length - 1;
+
+        while (left < right) {
+            if (height[left] == height[right]) {
+                water = Math.max(water, (right - left) * height[left]);
+                left++;
+                right--;
+            } else if (height[left] > height[right]) {
+                water = Math.max(water, (right - left) * height[right]);
+                right--;
+            } else {
+                water = Math.max(water, (right - left) * height[left]);
+                left++;
+            }
+        }
+        return water;
+    }
+}
