14e621d9937886c7c9f9caf681503dfb6604fe50
Tezla-Lee <tezla.lee@gmail.com>
2021-04-27 16:25:59 +0900

    solve: 프로그래머스 방금그곡
    - 문자열
    - 2018 카카오

--- Programmers/src/_2018_카카오블라인드채용/_3차_방금그곡/Solution.java
+++ Programmers/src/_2018_카카오블라인드채용/_3차_방금그곡/Solution.java
@@ -1,129 +1,78 @@
 package _2018_카카오블라인드채용._3차_방금그곡;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Solution {
     public String solution(String m, String[] musicinfos) {
-        String[] music;
-        StringBuilder sb;
+        Queue<Music> pq = new PriorityQueue<>();
+
+        for (int i = 0; i < musicinfos.length; i++) {
+            pq.offer(convertToMusic(musicinfos[i], i));
+        }
+
+        m = convertSharpToInt(m);
+
+        while (!pq.isEmpty()) {
+            Music music = pq.poll();
+
+            if (music.melody.contains(m)) {
+                return music.name;
+            }
+        }
+
+        return "(None)";
+    }
+
+    private Music convertToMusic(String music, int order) {
+        String[] info = music.split(",");
+        String[] startTime = info[0].split(":");
+        String[] endTime = info[1].split(":");
+
+        String name = info[2];
+        int playTime = (Integer.parseInt(endTime[0]) - Integer.parseInt(startTime[0])) * 60 + (Integer.parseInt(endTime[1]) - Integer.parseInt(startTime[1]));
+        String melody = getPlayingMelody(convertSharpToInt(info[3]), playTime);
+
+        return new Music(name, playTime, order, melody);
+    }
+
+    private String getPlayingMelody(String melody, int playTime) {
+        StringBuilder sb = new StringBuilder();
+        char[] chars = melody.toCharArray();
+
+        for (int i = 0; i < playTime / melody.length(); i++) {
+            sb.append(melody);
+        }
+
+        for (int i = 0; i < playTime % melody.length(); i++) {
+            sb.append(chars[i]);
+        }
+
+        return sb.toString();
+    }
+
+    private String convertSharpToInt(String s) {
+        return s.replaceAll("C#", "1")
+                .replaceAll("D#", "2")
+                .replaceAll("F#", "3")
+                .replaceAll("G#", "4")
+                .replaceAll("A#", "5");
+    }
+
+    static private class Music implements Comparable<Music> {
+        String name;
         int playTime;
-        int index = 0;
-        int count;
-        List<Music> musicList = new ArrayList<>();
-        for (String musicInfo : musicinfos) {
-            count = 0;
-            sb = new StringBuilder();
-            music = musicInfo.split(",");
-            playTime = Integer.parseInt(music[1].split(":")[0]) * 60 - Integer.parseInt(music[0].split(":")[0]) * 60 + Integer.parseInt(music[1].split(":")[1]) - Integer.parseInt(music[0].split(":")[1]);
-            if (playTime < 0) {
-                playTime += 60;
-            }
-            for (int i = 0; i < music[3].length(); i++) {
-                if (music[3].charAt(i) == '#') {
-                    count++;
-                }
-            }
+        int order;
+        String melody;
 
-            for (int i = 0; i < playTime / (music[3].length() - count); i++) {
-                sb.append(music[3]);
-            }
-
-            int count2;
-            int temp = 0;
-            while (true) {
-                count2 = 0;
-                String s = music[3].substring(0, playTime % (music[3].length() - count) + temp);
-                for (int i = 0; i < s.length(); i++) {
-                    if (s.charAt(i) == '#') {
-                        count2++;
-                    }
-                }
-                if (count2 == temp) {
-                    break;
-                }
-                temp = count2;
-            }
-            sb.append(music[3], 0, playTime % (music[3].length() - count) + count2);
-
-//            for (int i = 0; i < sb.toString().length(); i++) {
-//                Boolean check = true;
-//                if (sb.toString().charAt(i) == m.charAt(0)) {
-//                    for (int j = 0; j < m.length(); j++) {
-//                        if (i + j > sb.toString().length() - 1 || m.charAt(j) != sb.toString().charAt(i + j)) {
-//                            check = false;
-//                            break;
-//                        }
-//                    }
-//                    if (check && i + m.length() <= sb.toString().length() - 1 && sb.toString().charAt(i + m.length()) != '#') {
-//                        musicList.add(new Music(playTime, music[2], index++));
-//                    }
-//                }
-//            }
-
-            int count3 = 0;
-            while (sb.toString().indexOf(m, count3) + m.length() < sb.length()) {
-                if (sb.toString().indexOf(m, count3) != -1) {
-                    if (sb.toString().charAt(sb.toString().indexOf(m, count3) + m.length()) != '#') {
-                        musicList.add(new Music(playTime, music[2], index++));
-                        break;
-                    } else {
-                        count3 += m.length();
-                    }
-                } else {
-                    break;
-                }
-            }
-//            if (sb.toString().contains(m) && sb.toString().split(m).length > sb.toString().split(m + "#").length) {
-//                musicList.add(new Music(playTime, music[2], index++));
-//            }
-
-//            if (!sb.toString().contains(m + "#") && sb.toString().contains(m)) {
-//            musicList.add(new Music(playTime, music[2], index++));
-//            }
+        public Music(String name, int playTime, int order, String melody) {
+            this.name = name;
+            this.playTime = playTime;
+            this.order = order;
+            this.melody = melody;
         }
-        Collections.sort(musicList);
-        if (musicList.isEmpty()) {
-            return "(None)";
-        } else {
-            return musicList.get(0).name;
+
+        public int compareTo(Music o) {
+            return o.playTime == playTime ? order - o.order : o.playTime - playTime;
         }
-    }
-}
-
-class Music implements Comparable<Music> {
-    int playTIme;
-    int index;
-    String name;
-
-    public Music(int playTIme, String name, int index) {
-        this.index = index;
-        this.playTIme = playTIme;
-        this.name = name;
-    }
-
-//    @Override
-//    public String toString() {
-//        return "playTIme=" + playTIme +
-//                ", name='" + name + ", " + index + '\'' +
-//                '}';
-//    }
-
-    @Override
-    public int compareTo(Music o) {
-        if (playTIme == o.playTIme) {
-            return index - o.index;
-        }
-        return o.playTIme - playTIme;
-    }
-
-    public static void main(String[] args) {
-        System.out.println(new Solution().solution("ABCDEFG", new String[]{"13:00,13:23,GOOD,CDEFGABCDEFG#AB", "12:00,12:12,HELLO,CDEFGAB", "13:00,13:03,WORLD,ABCDEF", "12:00,12:14,BYE,CDEFGAB"}));
-        System.out.println(new Solution().solution("CC#BCC#BCC#BCC#B", new String[]{"03:00,03:30,FOO,CC#B", "04:00,04:08,BAR,CC#BCC#BCC#B"}));
-        System.out.println(new Solution().solution("ABC", new String[]{"12:00,12:14,HELLO,C#DEFGAB", "13:00,13:05,WORLD,ABCDEF"}));
-        System.out.println(new Solution().solution("CCB", new String[]{"03:00,03:10,FOO,CCB#CCB", "04:00,04:08,BAR,ABC"}));
-//        System.out.println(Arrays.toString("BCA#".split("BCA")));
-//        System.out.println(Arrays.toString("BCA#".split("BCA#")));
     }
 }
