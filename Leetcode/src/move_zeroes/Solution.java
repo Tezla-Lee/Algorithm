31d751d828136805daaed103e6f47c0d2430ffde
Jaebok Lee <Tezla.lee@gmail.com>
2021-05-28 23:44:13 +0900

    solve: Leetcode Moves Zero

---
+++ Leetcode/src/move_zeroes/Solution.java
@@ -0,0 +1,17 @@
+package move_zeroes;
+
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int index = 0;
+
+        for (int num : nums) {
+            if (num != 0) {
+                nums[index++] = num;
+            }
+        }
+
+        for (int i = index; i < nums.length; i++) {
+            nums[i] = 0;
+        }
+    }
+}
