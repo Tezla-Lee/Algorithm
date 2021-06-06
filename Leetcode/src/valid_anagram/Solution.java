d48c652ee0230130b39a18ea0a3ca6fe02061e88
Jaebok Lee <Tezla.lee@gmail.com>
2021-06-06 18:21:44 +0900

    solve: LeetCode valid anagram

---
+++ Leetcode/src/valid_anagram/Solution.java
@@ -0,0 +1,22 @@
+package valid_anagram;
+
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        int[] counts = new int[26];
+        for (char c : s.toCharArray()) {
+            counts[c - 'a']++;
+        }
+
+        for (char c : t.toCharArray()) {
+            counts[c - 'a']--;
+        }
+
+        for (int c : counts) {
+            if (c != 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
